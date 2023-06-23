75f91c7b5a7c2d11d231910c37d51c96a51e9dc2
AnhKieu0406 <luuanhkieu@gmail.com>
2023-06-23 09:34:55 +0700

    init MockProject

---
+++ Js/curron_male.js
@@ -0,0 +1,275 @@
+var listProduct = [
+  {
+    id: 1,
+    name: "WANDER",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_brown4_1_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+  {
+    id: 2,
+    name: "ASPIRE",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_green5_2.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+  {
+    id: 3,
+    name: "ALTER",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_white_blue8.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+  {
+    id: 4,
+    name: "FLOW",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fs%2Fh%2Fshift_-_1_.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+
+  {
+    id: 5,
+    name: "SHIFT",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fr%2Fo%2Frove_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+  {
+    id: 6,
+    name: "ROVE",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fv%2Fa%2Fvary_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "8.000.000",
+  },
+  {
+    id: 7,
+    name: "VARY",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Ft%2Fo%2Ftoil_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "6.000.000",
+  },
+  {
+    id: 8,
+    name: "TOLY",
+    sales: "-20%",
+    category: "WHITESANDS",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_brown4_1_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  
+  {
+    id: 9,
+    name: "HUNK",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fa%2F6%2Fa6-2.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 10,
+    name: "POTENT",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fb%2Ft%2Fbt.gallant.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 11,
+    name: "HUNK",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fv%2Fd%2Fvd.potent.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 12,
+    name: "APACHE",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+    "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fv%2Fd%2Fvd.dapper.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 13,
+    name: "FEARLESS",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+    "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fv%2Fd%2Fvd.dapper.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 14,
+    name: "REBEL",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_brown4_1_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 14,
+    name: "REBEL",
+    sales: "-20%",
+    category: "KASHMIR",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_brown4_1_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 15,
+    name: "APACHE",
+    sales: "-20%",
+    category: "G-FORK",
+    avatar:
+      "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2F1%2F_%2F1_brown4_1_1.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  {
+    id: 14,
+    name: "REBEL",
+    sales: "-20%",
+    category: "G-FORK",
+    avatar:
+     "https://curnonwatch.com/_next/image/?url=https%3A%2F%2Fshop.curnonwatch.com%2Fmedia%2Fcatalog%2Fproduct%2Fw%2Fe%2Fweb_-_hornet_du_1_1_.png&w=640&q=75",
+    priceBefore: "10.00.000",
+    priceAfter: "5.000.000",
+  },
+  
+];
+
+$(document).ready(function () {
+  logView();
+});
+function logView() {
+  for (var i = 0; i < listProduct.length; i++) {
+    let view2 = ` <div class="product-item w-25 position-rel ">
+                      <div class="product-cate position-rel">
+                          <div><img src="${listProduct[i].avatar}" class="w-100" alt="cate"></div>
+                          <div class="product-sale">${listProduct[i].sales}</div>
+                      </div>
+                      <div class="product-name text-center">
+                          <h4>${listProduct[i].category}</h4>
+                          <h3>${listProduct[i].name}</h3>
+                          <p>
+                              <span class="fw-900">${listProduct[i].priceBefore}</span>
+                              <span class="text-through">${listProduct[i].priceAfter}</span>
+                          </p>
+                      </div>
+                      <button>Thêm giỏ hàng</button>
+                      </div>
+                    </div>`;
+
+    $(".list-product").append(view2);
+  }
+}
+
+// function setView(arrProduct) {
+
+//   $(".list-product").html("");
+//   for (var i = 0;i< arrProduct.length ; i++) {
+//     renderHTML(arrProduct[i]);
+
+//   }
+// }
+
+// function renderHTML(obj) {
+//   let view2 = `<div class="product-item position-rel w-25">
+//                         <div class="top-product position-rel mb-2r">
+//                             <div class="product-image">
+//                                 <img class="w-100" src="${obj.avatar}" alt="image">
+//                             </div>
+//                             <div class="product-sale">
+//                                 <p class="font-15">${obj.sales}</p>
+//                             </div>
+//                         </div>
+//                         <div class="mid-product">
+//                             <h4 class="product-cate text-center mb-1r">${obj.category}</h4>
+//                             <h3 class="product-name text-center mb-1r">
+//                                 Name
+//                             </h3>
+//                             <div class="text-center mb-1r">
+//                                 <span>${obj.priceAfter} </span>
+//                                 <span>${obj.priceBefore}</span>
+//                             </div>
+//                         </div>
+//                         <div class="bot-product w-100">
+//                             <button class="btn-add w-100">
+//                                 Thêm vào giỏ hàng
+//                             </button>
+//                         </div>
+//                     </div>`;
+//   console.log("view2.... ", view2);
+//   $(".list-product").append(view2);
+// }
+
+//chức năng tìm kiếm
+//b1 click vào "tìm kiếm" => lấy value tong oinput search
+//b2: so sánh giá trị trong ô input search với tất cả các thuộc tính name trong array ds sản phẩm
+//b3 L nến có thuộc tính nam thỏa mãn thì in ra màn hình phần tử có thuộc tính name đó
+
+// var product = document.querySelector(".list-product");
+// listProduct.forEach(item => {
+//   var newProduct = `<div class="product-item position-rel w-25">
+//                             <div class="top-product position-rel mb-2r">
+//                                 <div class="product-image">
+//                                     <img class="w-100" src="${item.avatar}" alt="image">
+//                                 </div>
+//                                 <div class="product-sale">
+//                                     <p class="font-15">${item.sales}</p>
+//                                 </div>
+//                             </div>
+//                             <div class="mid-product">
+//                                 <h4 class="product-cate text-center mb-1r">${item.category}</h4>
+//                                 <h3 class="product-name text-center mb-1r">
+//                                    ${item.name}
+//                                 </h3>
+//                                 <div class="text-center mb-1r">
+//                                     <span>${item.priceAfter} </span>
+//                                     <span>${item.priceBefore}</span>
+//                                 </div>
+//                             </div>
+//                             <div class="bot-product w-100">
+//                                 <button class="btn-add w-100">
+//                                     Thêm vào giỏ hàng
+//                                 </button>
+//                             </div>
+//                            </div> `
+//         $(".list-product").append(newProduct);
+// });
